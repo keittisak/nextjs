3aeb56a33bbfc4c983cd0d10bd710f2e01304352
Keittisak <tay.shangrila@gmail.com>
2022-09-07 15:06:58 +0700

    child process

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import { spawn } from 'child_process';
+
 export default function handler(req, res) {
+  const ls = spawn('ls', ['-lh', '/usr']);
+  ls.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
   res.status(200).json({ name: 'John Doe' })
 }
