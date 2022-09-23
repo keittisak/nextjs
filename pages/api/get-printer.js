26034501a553a2fffc46327375c27fa7add1c42f
Keittisak <tay.shangrila@gmail.com>
2022-09-23 14:54:56 +0700

    fix lib usb

--- pages/api/get-printer.js
+++ pages/api/get-printer.js
@@ -2,10 +2,8 @@ import escpos from 'escpos';
 // import escposUsb from 'escpos-usb';
 import newEscposUsb from 'utils/new-escpos-usb';
 
-escpos.USB = newEscposUsb;
-
 export default function handler(req, res) {
-  let devices = escposUsb.findPrinter();
+  let devices = newEscposUsb.findPrinter();
 //   let device = devices[0];
   res.status(200).json(devices)
 }
