1de50c4df3d0624a171a39df4a6f3c2661abf98f
Keittisak <tay.shangrila@gmail.com>
2022-09-23 14:59:19 +0700

    test api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,23 +1,5 @@
-import escpos from 'escpos';
-import escposUsb from 'escpos-usb';
-
-escpos.USB = escposUsb;
 
 export default function handler(req, res) {
-  let devices = escposUsb.findPrinter();
-  // let device = new escpos.USB()
-  let device = {device:devices[0]};
-  const printer = new escpos.Printer(device);
-  device.open(function(){
-      printer
-      .font('a')
-      .align('ct')
-      .style('bu')
-      .size(1, 1)
-      .text('The quick brown fox jumps over the lazy dog')
-      .cut()
-      .close()
-  });
-  res.status(200).json(printer)
+  res.status(200).json({message:'Hellor'})
 }
 
