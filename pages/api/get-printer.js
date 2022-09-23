36eb2807bbbe26fb6ee84f685fa12929dff63d10
Keittisak <tay.shangrila@gmail.com>
2022-09-23 15:32:22 +0700

    fix

--- pages/api/get-printer.js
+++ pages/api/get-printer.js
@@ -1,10 +1,7 @@
-import escpos from 'escpos';
-// import escposUsb from 'escpos-usb';
 import newEscposUsb from 'utils/new-escpos-usb';
 
 export default function handler(req, res) {
   let devices = newEscposUsb.findPrinter();
-//   let device = devices[0];
   res.status(200).json(devices)
 }
 
